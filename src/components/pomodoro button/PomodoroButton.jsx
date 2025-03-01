c9e4f2cc3e02af900728dfc251249eda9cd45d0b
Deras879 <derycard59@gmail.com>
2025-02-28 19:24:44 -0500

    Mejorados estilos

--- src/components/pomodoro button/PomodoroButton.jsx
+++ src/components/pomodoro button/PomodoroButton.jsx
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
 import "./PomodoroButton.css";
 
-const PomodoroButton = ({ label }) => {
-  const [isActive, setIsActive] = useState(false);
-
-  const handleClick = () => {
-    setIsActive(!isActive);
-  };
+const PomodoroButton = ({ label, status, onClick }) => {
+  // const [isActive, setIsActive] = useState(false);
 
   return (
     <button
-      className={`pomodoro-button${isActive ? "-active" : ""}`}
-      onClick={handleClick}
+      className={`pomodoro-button${status ? "-active" : ""}`}
+      onClick={onClick}
     >
       {label}
     </button>
--- src/views/mainview/MainView.jsx
+++ src/views/mainview/MainView.jsx
@@ -4,13 +4,31 @@ import { useState } from "react";
 import PomodoroButton from "../../components/pomodoro button/PomodoroButton";
 
 const MainView = () => {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonClick = (id) => {
+    setActiveButton(id);
+  };
+
   return (
     <div>
       <div className="Major-container">
         <div className="Buttons">
-          <PomodoroButton label="Pomodoro" />
-          <PomodoroButton label="Short Break" />
-          <PomodoroButton label="Long Break" />
+          <PomodoroButton
+            status={activeButton === 1}
+            label="Pomodoro"
+            onClick={() => handleButtonClick(1)}
+          />
+          <PomodoroButton
+            status={activeButton === 2}
+            label="Short Break"
+            onClick={() => handleButtonClick(2)}
+          />
+          <PomodoroButton
+            status={activeButton === 3}
+            label="Long Break"
+            onClick={() => handleButtonClick(3)}
+          />
         </div>
         <div className="Timer">25:00</div>
 
